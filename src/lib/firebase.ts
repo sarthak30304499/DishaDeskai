cb4f183019bd7c68921fa46b63410df31935f64d
sarthak palgotra <thesarthakpalgotra@gmail.com>
2025-08-21 21:33:45 +0000

    File changes

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -7,12 +7,12 @@ import { getAuth } from 'firebase/auth';
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  projectId: 'dishadesk',
+  appId: '1:781150870360:web:6dbf9958e464d163cb632d',
+  storageBucket: 'dishadesk.firebasestorage.app',
+  apiKey: 'AIzaSyDaZJUWoifY838DCQm1oYPHFkz_J55LjsM',
+  authDomain: 'dishadesk.firebaseapp.com',
+  messagingSenderId: '781150870360',
 };
 
 // Initialize Firebase
